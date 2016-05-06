cec13d509da38cb435e8a0b98853506850ac7ddf
64007517 <64007517@hs-61642-macair.ad.edenpr.org>
2016-05-06 12:42:59 -0500

    Update 1.0.0:Base for viewing pictures

---
+++ src/pictureEdit/PictureViewer.java
@@ -0,0 +1,11 @@
+package pictureEdit;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.*;
+import javax.swing.border.*;
+
+public class PictureViewer {
+
+}
