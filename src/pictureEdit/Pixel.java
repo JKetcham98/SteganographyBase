4eb06e36196eb3d2f8d2ad94ee439aadb0473450
Joshua Ketcham <90299352@ep-student.org>
2016-05-10 12:17:43 -0500

    1.0.1
    Added pixel class

--- src/pictureEdit/Pixel.java
+++ src/pictureEdit/Pixel.java
@@ -2,6 +2,11 @@ package pictureEdit;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.*;
+import java.io.*;
+import java.awt.geom.*;
 
 public class Pixel {
 //instance variables
@@ -11,10 +16,56 @@ public class Pixel {
 	private int y;//y location(row)
 
 //constructor
-	public Pixel(BufferedImage picture, int x; int y){
+	public Pixel(BufferedImage picture, int x, int y){
 		this.picture=picture;
 		this.x=x;
 		this.y=y;
 	}
 	
+//methods
+	public int getX(){//gives x position
+		return x;
+	}
+	public int getY(){//gives y postion
+		return y;
+	}
+	public void setX(int a){//sets x position
+		this.x=a;
+	}
+	public void setY(int a){//sets y position
+		this.y=a;
+	}
+	//returns specific values from a 32 bit integer that represents a RGBA color
+	public int getAlpha() {//gives alpha
+		int value = picture.getRGB(x,y);//gives A value
+		return (value >> 24) & 0xff;
+	}
+	public int getRed(){//gives red 0-255
+    	int value = picture.getRGB(x,y);//gives R value
+    	return (value >> 16) & 0xff;
+	}
+	public int getGreen(){//gives green 0-255
+    	int value = picture.getRGB(x,y);//gives G value
+    	return (value >>  8) & 0xff;
+	}
+	public int getBlue(){//gives blue 0-255
+    	int value = picture.getRGB(x,y);//gives B value
+    	return value & 0xff;
+	}
+	public void setColor(int alpha, int red, int green, int blue){
+	    int value = (alpha << 24) + (red << 16) + (green << 8) + blue;
+	    picture.setRGB(this.x,this.y,value);//might not work, try without (this.) if it does not work.
+	}//the stuff below uses setColor to set colors for specific values
+	public void setRed(int value){
+		setColor(getAlpha(), value, getGreen(), getBlue());
+	}
+	public void setGreen(int value){
+		setColor(getAlpha(), getRed(), value, getBlue());
+	}
+	public void setBlue(int value){
+		setColor(getAlpha(), getRed(), getGreen(), value);
+	}
+	public String toString(){
+	    return "Y= " + getY() + "\nX= " + getX() +"\nAlpha,Red,Green,Blue= " + getAlpha()+","+ getRed() + "," + getGreen() + "," + getBlue();
+	}
 }
