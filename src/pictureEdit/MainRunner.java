a0009accd89bdc158ee9a754d79afca7078bee21
Joshua Ketcham <90299352@ep-student.org>
2016-05-23 12:56:41 -0500

    Finished converters Preparing to start picture encoding.

--- src/pictureEdit/MainRunner.java
+++ src/pictureEdit/MainRunner.java
@@ -4,21 +4,27 @@ public class MainRunner {
 	public static void main(String[] args) {
 		//Picture pic = new Picture("BaseNaturePicture.jpg");
 		//pic.explore();
-		
-		System.out.println(textToBinary("How are you today?"));
-		System.out.println(binaryToText("01000001"));
-		System.out.println(textToBinary("A"));
-		System.out.println(toBaseTen(11111111));
-		System.out.println(stringToInt("100"));
-		System.out.println(toBinary(255));
-		
+		String binphrase=textToBinary("How are you today?");
+		System.out.println(binphrase);
+		System.out.println(decode(binphrase));
 	}
 	
 	
 	
 	
 	
-	public static String binaryToText(String num){//broken
+	//binary conversion methods
+	public static String decode(String str){
+		return reverseString(binaryToText(str));
+	}
+	public static String reverseString(String str){
+		String temp = "";
+		for(int pos=str.length()-1;pos>=0;pos--){
+			temp+=str.charAt(pos);
+		}
+		return temp;
+	}
+	public static String binaryToText(String num){
 		String phrase = "";
 		String temp="";
 		for(int spot=0;spot<num.length();spot++){
@@ -30,17 +36,17 @@ public class MainRunner {
 		}
 		return phrase;
 	}
-	public static int stringToInt(String str){//broken
+	public static int stringToInt(String str){
 		int num=0;
 		for(int index=0;index<str.length();index++){
-			num+= ((int)(str.charAt(index)-48)*Math.pow(10,index));
+			num *= 10;
+	        num += str.charAt(index) - 48;
 		}
 		return (int)num;
 	}
-	
 	public static String textToBinary(String phrase){
 		String num = "";
-		for(int index=0;index<phrase.length();index++){
+		for(int index=phrase.length()-1;index>=0;index--){
 			if((int)(phrase.charAt(index))<128){//for letters our converter will be using 8 bit numbers,
 				num+=0;//so our we need extra zeros in the front to decode correctly
 			}
@@ -51,7 +57,6 @@ public class MainRunner {
 		}//then concatenates that on a string called num
 		return num;
 	}
-	
 	public static int toBinary(int number){//can only accept values underneath 512 exclusive
 		double temp=0;//temp value holder
 		int place=1;//marks the place slot
@@ -62,7 +67,6 @@ public class MainRunner {
 		}
 		return (int)temp/10;
 	}
-	
 	public static int toBaseTen(int number){//converts binary to base 10
 		double temp=0;//temp value holder
 		int place=0;//marks the place slot
